7639c2bf870f12bba49710a24a2416d07cedce21
natsuto-kun <natsuto391@gmail.com>
2021-01-17 15:44:17 +0900

    modalの実装

--- src/Form.js
+++ src/Form.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Button } from "./components/button";
 import { TabBodyContainer } from "./components/tab-body-container";
 import styled from 'styled-components';
+import FormModal from './FormModal';
 
 const Label = styled.label`
   display: flex;
@@ -26,10 +27,11 @@ const FormButton = styled(Button)`
 
 export const Form = ({ onAddLang }) => {
   const [text, setText] = useState('');
+  const [showModal, setShowModal] = useState(false);
 
   const submitForm = (e) => {
     e.preventDefault();
-    onAddLang(text);
+    setShowModal(true);
   }
 
   return (
@@ -43,6 +45,13 @@ export const Form = ({ onAddLang }) => {
           <FormButton>追加</FormButton>
         </ButtonContainer>
       </form>
+      {
+        showModal &&
+        <FormModal
+          confirm={() => onAddLang(text)}
+          cancel={() => setShowModal(false)}
+        />
+      }
     </TabBodyContainer>
   )
 }
